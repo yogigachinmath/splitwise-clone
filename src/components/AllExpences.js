5ef4acea188d36e8439d3bdae2b2315ec8cba5ac
Arun Prajapati <arun.prajapati@mountblue.io>
2019-10-17 10:51:07 +0530

    remove unnecessary comment

--- src/components/AllExpences.js
+++ src/components/AllExpences.js
@@ -165,67 +165,6 @@ export class DashboardMain extends Component {
               {Object.keys(this.state.expenses).map(expense => {
                 return (
                   <Expense expense={this.state.expenses[expense]} />
-                  // <div className="expense" onClick={this.showDetails}>
-                  //   <div className="row expence-row line-height-18 border border-top-0 border-left-0 border-right-0">
-                  //     <div className="d-flex justify-content-between container">
-                  //       <div className="d-flex expenses-child ">
-                  //         <div className="d-flex flex-column mr-2 text-secondary">
-                  //           <small>Oct</small>
-                  //           <h5 className="line-height-18">14</h5>
-                  //         </div>
-                  //         <img
-                  //           className="expense-img mr-2"
-                  //           src="/img/general@2x.png"
-                  //           alt=""
-                  //         />
-                  //         <div className="d-flex flex-column">
-                  //           <a
-                  //             className="expense-heading text-truncate font-weight-bold"
-                  //             href="/"
-                  //           >
-                  //             {this.state.expenses[expense].description}
-                  //           </a>
-                  //           {this.state.expenses[expense].groupId ? (
-                  //             <a href="/">
-                  //               <small className="text-secondary group-expense-link">
-                  //                 Hacker
-                  //               </small>
-                  //             </a>
-                  //           ) : (
-                  //             ""
-                  //           )}
-                  //         </div>
-                  //       </div>
-                  //       <div className="d-flex justify-content-between expenses-child">
-                  //         <div className="d-flex flex-column mr-3">
-                  //           <small className="align-self-sm-end text-secondary">
-                  //             you paid
-                  //           </small>
-                  //           <span className="font-weight-bold">
-                  //             INR {this.state.expenses[expense].cost}
-                  //           </span>
-                  //         </div>
-                  //         <div className="d-flex flex-column ">
-                  //           <small className="text-secondary">you lent</small>
-                  //           <span className="font-weight-bold lent-color">
-                  //             INR
-                  //             {this.state.expenses[expense].repayments.reduce(
-                  //               (lentAmount, repayment) => {
-                  //                 let totalAmount = lentAmount;
-                  //                 totalAmount += repayment.amount;
-                  //                 return totalAmount;
-                  //               },
-                  //               0
-                  //             )}
-                  //           </span>
-                  //         </div>
-                  //       </div>
-                  //     </div>
-                  //   </div>
-                  //   {this.state.showExpenseDetails ? (
-                  //     <ExpenseDetail expense={this.state.expenses[expense]} />
-                  //   ) : null}
-                  // </div>
                 );
               })}
             </div>

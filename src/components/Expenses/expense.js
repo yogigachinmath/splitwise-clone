114df8e4708027f36c2e4dc8406adc9fc1e6253e
Arun Prajapati <arun.prajapati@mountblue.io>
2019-10-23 04:40:58 +0530

    create sidebar for friends

--- src/components/Expenses/expense.js
+++ src/components/Expenses/expense.js
@@ -186,219 +186,257 @@ class Expense extends Component {
     });
     return expenses;
   }
+  getUserBalance(expenses) {
+    const userBalance = expenses.reduce((totalBalance, expense) => {
+      totalBalance += expense.users[this.props.user.uid].netBalance;
+      return totalBalance;
+    }, 0);
+    return userBalance;
+  }
 
   render() {
+    const userBalance = this.getUserBalance(
+      this.getExpensesWithFriend(this.props.expensesData)
+    );
     return (
-      <div className="dash-main-content col-md-6">
-        {/* MOdal */}
-        <form onSubmit={this.onSubmit}>
-          <div
-            className="modal fade"
-            id="exampleModalCenter"
-            tabindex="-1"
-            role="dialog"
-            aria-labelledby="exampleModalCenterTitle"
-            aria-hidden="true"
-          >
-            <div className="modal-dialog modal-dialog-centered" role="document">
-              <div className="modal-content">
-                <div className="modal-header" style={{ background: "#5cc5a7" }}>
-                  <h6 className="modal-title" id="exampleModalCenterTitle">
-                    Add an expense
-                  </h6>
-                  <button
-                    type="button"
-                    className="close"
-                    data-dismiss="modal"
-                    style={{ color: "white" }}
-                    aria-label="Close"
+      <React.Fragment>
+        <div className="dash-main-content col-md-6">
+          {/* MOdal */}
+          <form onSubmit={this.onSubmit}>
+            <div
+              className="modal fade"
+              id="exampleModalCenter"
+              tabindex="-1"
+              role="dialog"
+              aria-labelledby="exampleModalCenterTitle"
+              aria-hidden="true"
+            >
+              <div
+                className="modal-dialog modal-dialog-centered"
+                role="document"
+              >
+                <div className="modal-content">
+                  <div
+                    className="modal-header"
+                    style={{ background: "#5cc5a7" }}
                   >
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <div className="modal-body">
-                  <p className="modal-title" id="exampleModalCenterTitle">
-                    With <b>you</b> and <b>{this.props.match.params.name}</b>
-                  </p>
-                  <hr />
-                  <div className="infoDesc">
-                    <img src="/img/desc.png" />
-                    <div className="main-content">
-                      <input
-                        type="text"
-                        className="description ml-3 mb-4"
-                        name={"currentdesc"}
-                        onChange={this.handleChange}
-                        value={this.state.currentdesc}
-                        placeholder="Enter a description"
-                        style={{ "font-size": "20px" }}
-                      />
-                      <div className="cost-conatiner ml-3">
-                        <span className="currency_code">Rs</span>
-                        <input
-                          type="number"
-                          className="ml-1 mb-2"
-                          name={"currentamount"}
-                          onChange={this.handleChange}
-                          placeholder="0.00"
-                          value={this.state.currentamount}
-                        />
-                      </div>
-                    </div>
-                  </div>
-                  <div className="splitAmount mt-3">
-                    <div className="row mb-3">
-                      <button
-                        className="btn btn-secondary mr-2"
-                        onClick={this.showSplitEqually}
-                      >
-                        =
-                      </button>
-                      <button
-                        className="btn btn-secondary mr-2"
-                        onClick={this.showSplitExact}
-                      >
-                        RS. 1.50
-                      </button>
-                      <button
-                        className="btn btn-secondary"
-                        onClick={this.showSplitPercentage}
-                      >
-                        %
-                      </button>
-                    </div>
-                    <div className="splitEqually">
-                      <h5>Split Equally</h5>
-                      <div className="innerDetails m-3">
-                        <span>You</span>
-                        <input
-                          type="number"
-                          className="description ml-1 mb-2"
-                          name={"curruser1"}
-                          onChange={this.handleChange}
-                          placeholder="0.00"
-                          value={this.state.curruser1}
-                          readOnly
-                        />
-                      </div>
-                      <div className="innerDetails m-3">
-                        <span>{this.props.match.params.name}</span>
-                        <input
-                          type="number"
-                          className="description ml-1 mb-2"
-                          name={"curruser2"}
-                          onChange={this.handleChange}
-                          placeholder="0.00"
-                          value={this.state.curruser2}
-                          readOnly
-                        />
-                      </div>
-                    </div>
-                    <div className="splitExact" style={{ display: "none" }}>
-                      <h5>Split by exact amounts</h5>
-                      <div className="innerDetails m-3">
-                        <span>You</span>
-                        <input
-                          type="number"
-                          className="description ml-1 mb-2"
-                          name={"curruser1"}
-                          onChange={this.handleChange}
-                          placeholder="0.00"
-                          value={this.state.curruser1}
-                          readOnly
-                        />
-                      </div>
-                      <div className="innerDetails m-3">
-                        <span>{this.props.match.params.name}</span>
-                        <input
-                          type="number"
-                          className="description ml-1 mb-2"
-                          name={"curruser2"}
-                          onChange={this.handleChange}
-                          placeholder="0.00"
-                          value={this.state.curruser2}
-                        />
-                      </div>
-                    </div>
-                    <div
-                      className="splitPercentage"
-                      style={{ display: "none" }}
+                    <h6 className="modal-title" id="exampleModalCenterTitle">
+                      Add an expense
+                    </h6>
+                    <button
+                      type="button"
+                      className="close"
+                      data-dismiss="modal"
+                      style={{ color: "white" }}
+                      aria-label="Close"
                     >
-                      <h5>Split By Percentage</h5>
-                      <div className="innerDetails m-3">
-                        <span>You</span>
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                  <div className="modal-body">
+                    <p className="modal-title" id="exampleModalCenterTitle">
+                      With <b>you</b> and <b>{this.props.match.params.name}</b>
+                    </p>
+                    <hr />
+                    <div className="infoDesc">
+                      <img src="/img/desc.png" />
+                      <div className="main-content">
                         <input
-                          type="number"
-                          className="description ml-1 mb-2"
-                          name={"curruser1"}
+                          type="text"
+                          className="description ml-3 mb-4"
+                          name={"currentdesc"}
                           onChange={this.handleChange}
-                          placeholder="0.00"
-                          value={this.state.curruser1}
+                          value={this.state.currentdesc}
+                          placeholder="Enter a description"
+                          style={{ "font-size": "20px" }}
                         />
+                        <div className="cost-conatiner ml-3">
+                          <span className="currency_code">Rs</span>
+                          <input
+                            type="number"
+                            className="ml-1 mb-2"
+                            name={"currentamount"}
+                            onChange={this.handleChange}
+                            placeholder="0.00"
+                            value={this.state.currentamount}
+                          />
+                        </div>
                       </div>
-                      <div className="innerDetails m-3">
-                        <span>{this.props.match.params.name}</span>
-                        <input
-                          type="number"
-                          className="description ml-1 mb-2"
-                          name={"curruser2"}
-                          onChange={this.handleChange}
-                          placeholder="0.00"
-                          value={this.state.curruser2}
-                        />
+                    </div>
+                    <div className="splitAmount mt-3">
+                      <div className="row mb-3">
+                        <button
+                          className="btn btn-secondary mr-2"
+                          onClick={this.showSplitEqually}
+                        >
+                          =
+                        </button>
+                        <button
+                          className="btn btn-secondary mr-2"
+                          onClick={this.showSplitExact}
+                        >
+                          RS. 1.50
+                        </button>
+                        <button
+                          className="btn btn-secondary"
+                          onClick={this.showSplitPercentage}
+                        >
+                          %
+                        </button>
+                      </div>
+                      <div className="splitEqually">
+                        <h5>Split Equally</h5>
+                        <div className="innerDetails m-3">
+                          <span>You</span>
+                          <input
+                            type="number"
+                            className="description ml-1 mb-2"
+                            name={"curruser1"}
+                            onChange={this.handleChange}
+                            placeholder="0.00"
+                            value={this.state.curruser1}
+                            readOnly
+                          />
+                        </div>
+                        <div className="innerDetails m-3">
+                          <span>{this.props.match.params.name}</span>
+                          <input
+                            type="number"
+                            className="description ml-1 mb-2"
+                            name={"curruser2"}
+                            onChange={this.handleChange}
+                            placeholder="0.00"
+                            value={this.state.curruser2}
+                            readOnly
+                          />
+                        </div>
+                      </div>
+                      <div className="splitExact" style={{ display: "none" }}>
+                        <h5>Split by exact amounts</h5>
+                        <div className="innerDetails m-3">
+                          <span>You</span>
+                          <input
+                            type="number"
+                            className="description ml-1 mb-2"
+                            name={"curruser1"}
+                            onChange={this.handleChange}
+                            placeholder="0.00"
+                            value={this.state.curruser1}
+                            readOnly
+                          />
+                        </div>
+                        <div className="innerDetails m-3">
+                          <span>{this.props.match.params.name}</span>
+                          <input
+                            type="number"
+                            className="description ml-1 mb-2"
+                            name={"curruser2"}
+                            onChange={this.handleChange}
+                            placeholder="0.00"
+                            value={this.state.curruser2}
+                          />
+                        </div>
+                      </div>
+                      <div
+                        className="splitPercentage"
+                        style={{ display: "none" }}
+                      >
+                        <h5>Split By Percentage</h5>
+                        <div className="innerDetails m-3">
+                          <span>You</span>
+                          <input
+                            type="number"
+                            className="description ml-1 mb-2"
+                            name={"curruser1"}
+                            onChange={this.handleChange}
+                            placeholder="0.00"
+                            value={this.state.curruser1}
+                          />
+                        </div>
+                        <div className="innerDetails m-3">
+                          <span>{this.props.match.params.name}</span>
+                          <input
+                            type="number"
+                            className="description ml-1 mb-2"
+                            name={"curruser2"}
+                            onChange={this.handleChange}
+                            placeholder="0.00"
+                            value={this.state.curruser2}
+                          />
+                        </div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div className="modal-footer">
-                  <button
-                    type="button"
-                    className="btn btn-secondary"
-                    data-dismiss="modal"
-                  >
-                    Close
-                  </button>
-                  <button type="submit" className="btn btn-primary">
-                    Save changes
-                  </button>
+                  <div className="modal-footer">
+                    <button
+                      type="button"
+                      className="btn btn-secondary"
+                      data-dismiss="modal"
+                    >
+                      Close
+                    </button>
+                    <button type="submit" className="btn btn-primary">
+                      Save changes
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </form>
-        {/* modal */}
-        <div className="dash-header p-3">
-          <div className="row">
-            <h4 className="mr-auto">{this.props.match.params.name}</h4>
-            <div className="dash-header-right ml-auto">
-              <button
-                type="button"
-                className="btn btn-orange"
-                data-toggle="modal"
-                data-target="#exampleModalCenter"
-              >
-                Add an expense
-              </button>
-              <button className="btn btn-blue ml-2">Settle up</button>
+          </form>
+          {/* modal */}
+          <div className="dash-header p-3">
+            <div className="row">
+              <h4 className="mr-auto">{this.props.match.params.name}</h4>
+              <div className="dash-header-right ml-auto">
+                <button
+                  type="button"
+                  className="btn btn-orange"
+                  data-toggle="modal"
+                  data-target="#exampleModalCenter"
+                >
+                  Add an expense
+                </button>
+                <button className="btn btn-blue ml-2">Settle up</button>
+              </div>
             </div>
           </div>
+          {this.props.expensesData.length > 0 &&
+          this.getExpensesWithFriend(this.props.expensesData).length > 0 ? (
+            this.sortExpenses(
+              this.getExpensesWithFriend(this.props.expensesData)
+            ).map(expense => {
+              return (
+                <ShowExpense
+                  expense={expense}
+                  currentUser={this.props.user.uid}
+                />
+              );
+            })
+          ) : (
+            <NoExpenses />
+          )}
         </div>
-        {this.props.expensesData.length > 0 &&
-        this.getExpensesWithFriend(this.props.expensesData).length > 0 ? (
-          this.sortExpenses(
-            this.getExpensesWithFriend(this.props.expensesData)
-          ).map(expense => {
-            return (
-              <ShowExpense
-                expense={expense}
-                currentUser={this.props.user.uid}
-              />
-            );
-          })
-        ) : (
-          <NoExpenses />
-        )}
-      </div>
+        <div className="right-sidebar col-md-3 p-3">
+          <small className="text-secondary font-weight-bold font-size-13">
+            YOUR BALANCE
+          </small>
+          {userBalance === 0 ? (
+            <div className="d-flex flex-column mt-2 text-secondary">
+              <span>You are all settled up</span>
+            </div>
+          ) : userBalance > 0 ? (
+            <div className="d-flex flex-column mt-2 colorBlue">
+              <span className="">{this.props.match.params.name} owes you</span>
+              <h3 className="font-weight-bold">&#x20b9;{userBalance}</h3>
+            </div>
+          ) : (
+            <div className="d-flex flex-column mt-2 orange-color">
+              <span className="">You owe {this.props.match.params.name}</span>
+              <h3 className="font-weight-bold">&#x20b9;{-userBalance}</h3>
+            </div>
+          )}
+        </div>
+      </React.Fragment>
     );
   }
 }
